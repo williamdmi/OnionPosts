9f132fe54c3a0bd9bf20c80786b3455867c419bc
WilliamDmi <williamdemitrienko31@gmail.com>
2022-09-14 22:11:43 +0300

    Successfully created post object array

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ const http = require('http');
 const fs = require('fs');
 const HttpProxyAgent = require('http-proxy-agent');
 const jsdom = require("jsdom");
+const { log } = require('console');
 const { JSDOM } = jsdom;
-
 
 // HTTP/HTTPS proxy to connect to
 let proxy = process.env.http_proxy || 'http://localhost:8118';
 console.log('using proxy server %j', proxy);
 
-// HTTP endpoint for the proxy to connect to
-let endpoint = process.argv[2] || 'http://paste2vljvhmwq5zy33re2hzu4fisgqsohufgbljqomib2brzx3q4mid.onion/lists';
-console.log('attempting to GET %j', endpoint);
-let opts = url.parse(endpoint);
 
-// create an instance of the `HttpProxyAgent` class with the proxy server information
-let agent = new HttpProxyAgent(proxy);
-opts.agent = agent;
+const fetchPages = (position) => {
 
-const fetchPages = () => {
+    // HTTP endpoint for the proxy to connect to
+    let endpoint = process.argv[2] || 'http://paste2vljvhmwq5zy33re2hzu4fisgqsohufgbljqomib2brzx3q4mid.onion/lists/' + position;
+    console.log('attempting to GET %j', endpoint);
+    let opts = url.parse(endpoint);
+
+    // create an instance of the `HttpProxyAgent` class with the proxy server information
+    let agent = new HttpProxyAgent(proxy);
+    opts.agent = agent;
+
     return new Promise((resolve, reject) => {
         let result = http.get(opts, (res) => {
             let html = '';
@@ -40,12 +42,20 @@ const fetchPages = () => {
 
 
 async function main() {
-    let htmlString = await fetchPages();
+    let htmlString = await fetchPages(0);
     fs.writeFile('myFile', htmlString, e => console.log(e))
     let dom = new JSDOM(htmlString);
-    let trArray = dom.window.document.querySelectorAll('tr');
-    trArray.forEach(element => 
-        console.log(element.innerHTML));
+    let trArray = Array.from(dom.window.document.querySelectorAll('tr'));
+    let posts = trArray.map(element => {
+        const children = element.children;
+        const postObject = {
+            title : children[0].textContent,
+            name : children[1].textContent,
+            when : children[3].textContent
+        };
+        return postObject;
+    });
+    console.log(posts);
 }
 
 main();
