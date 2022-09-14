64a951007307c34993b3d1bc02f4f1ea48df78cb
WilliamDmi <williamdemitrienko31@gmail.com>
2022-09-14 21:43:04 +0300

    Fetching with a promise now to get the string

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const url = require('url');
 const http = require('http');
 const fs = require('fs');
 const HttpProxyAgent = require('http-proxy-agent');
-const { log } = require('console');
-const DOMParser = require('xmldom').DOMParser;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 
 // HTTP/HTTPS proxy to connect to
@@ -11,7 +11,7 @@ let proxy = process.env.http_proxy || 'http://localhost:8118';
 console.log('using proxy server %j', proxy);
 
 // HTTP endpoint for the proxy to connect to
-let endpoint = process.argv[2] || 'http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all';
+let endpoint = process.argv[2] || 'http://paste2vljvhmwq5zy33re2hzu4fisgqsohufgbljqomib2brzx3q4mid.onion/lists';
 console.log('attempting to GET %j', endpoint);
 let opts = url.parse(endpoint);
 
@@ -19,20 +19,33 @@ let opts = url.parse(endpoint);
 let agent = new HttpProxyAgent(proxy);
 opts.agent = agent;
 
-async function fetchPages() {
-    let html = '', domHtml = null;
-    http.get(opts, (res) => {
-        // console.log('"response" event!', res.headers);
-        res.on('data', function (chunk) {
-            html += ("" + chunk)
-            console.log('' + chunk);
-            fs.writeFile('myFile',html, e => console.log(e));
+const fetchPages = () => {
+    return new Promise((resolve, reject) => {
+        let result = http.get(opts, (res) => {
+            let html = '';
+            res.on('data', function (chunk) {
+                html += ("" + chunk)
+            });
+            res.on('error', error => {
+                console.error(error);
+                reject(error);
+            });
+            res.on("end", d => {
+                resolve(html);
+            });
         });
-        let parser = new DOMParser();
-        domHtml = parser.parseFromString(html, 'text/html');
-    });
+        result.end();
+    })
 }
-function main() {
-    fetchPages();
+
+
+async function main() {
+    let htmlString = await fetchPages();
+    fs.writeFile('myFile', htmlString, e => console.log(e))
+    let dom = new JSDOM(htmlString);
+    let trArray = dom.window.document.querySelectorAll('tr');
+    trArray.forEach(element => 
+        console.log(element.innerHTML));
 }
+
 main();
