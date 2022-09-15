0866220db344ed1f789e014aab82ddbedfae925b
WilliamDmi <williamdemitrienko31@gmail.com>
2022-09-15 09:50:24 +0300

    Added comments

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ let proxy = process.env.http_proxy || 'http://localhost:8118';
 console.log('using proxy server %j', proxy);
 
 
-const fetchPages = (position) => {
-
+const fetchPageByPosition = (position) => {
     // HTTP endpoint for the proxy to connect to
     let endpoint = process.argv[2] || 'http://paste2vljvhmwq5zy33re2hzu4fisgqsohufgbljqomib2brzx3q4mid.onion/lists/' + position;
     console.log('attempting to GET %j', endpoint);
@@ -25,13 +24,16 @@ const fetchPages = (position) => {
     return new Promise((resolve, reject) => {
         let result = http.get(opts, (res) => {
             let html = '';
+            //Get the data and add to the html
             res.on('data', function (chunk) {
                 html += ("" + chunk)
             });
+            //Catch errors
             res.on('error', error => {
                 console.error(error);
                 reject(error);
             });
+            //When all the data is done resolve the promise with the data
             res.on("end", d => {
                 resolve(html);
             });
@@ -42,12 +44,14 @@ const fetchPages = (position) => {
 
 
 async function main() {
+    //Position of the post in the website
     let position = 0;
     let posts = [];
     while (true) {
-        let htmlString = await fetchPages(position);
+        let htmlString = await fetchPageByPosition(position);
         let dom = new JSDOM(htmlString);
         let trArray = Array.from(dom.window.document.querySelectorAll('tr'));
+        //If there are no posts go the the else statement and break
         if (trArray.length > 0) {
             let pagePosts = trArray.map(element => {
                 const children = element.children;
@@ -59,6 +63,7 @@ async function main() {
                 return postObject;
             });
             posts = posts.concat(pagePosts);
+            //The wesbsite has 50 posts per page so +50 goes to the next page
             position += 50;
         }
         else break;
